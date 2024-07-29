e234d9a6f1e34a3fcb919b6055d302c6543ad757
Mohamed Abukar <mo-abukar10@hotmail.com>
2024-07-29 14:21:59 +0100

    add prometheus test helm chart

---
+++ test-helm.tf
@@ -0,0 +1,10 @@
+resource "helm_release" "prometheus" {
+  name       = "prometheus"
+  repository = "https://prometheus-community.github.io/helm-charts"
+  chart      = "prometheus"
+
+  create_namespace = true
+  namespace        = "prometheus"
+}
+
+## working helm chart install via TF
